3fe1e70ad62aa4d9321985cc7fe99bcd514c9515
Minkyoung <choi90326@gmail.com>
2025-12-30 16:43:34 +0900

    vite base add

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // ✅ 추가
 import App from './App.jsx';
 import './index.css';
 import './styles/tokens.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
-      <App />
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
+         <App />
+      </BrowserRouter>
    </React.StrictMode>
 );
