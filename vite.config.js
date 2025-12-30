7950fc0bb528372012cbbbca8cb71791d70a60c8
Minkyoung <choi90326@gmail.com>
2025-12-30 16:39:51 +0900

    vite base add

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+   plugins: [react()],
+   base: '/Planify/', // ✅ 깃허브 레포 이름이랑 똑같이
+});
